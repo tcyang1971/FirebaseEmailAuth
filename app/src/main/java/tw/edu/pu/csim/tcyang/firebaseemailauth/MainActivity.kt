7f98012abcd2c9cffa7c09cdee5a01638bde2cc0
tcyang <tcyang@gm.pu.edu.tw>
2021-05-04 09:11:21 +0800

    獲取FirebaseAuth對象的共享實例

--- app/src/main/java/tw/edu/pu/csim/tcyang/firebaseemailauth/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firebaseemailauth/MainActivity.kt
@@ -2,10 +2,24 @@ package tw.edu.pu.csim.tcyang.firebaseemailauth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    lateinit var email:String
+    lateinit var password:String
+    lateinit var flag:String
+    var UID:String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 獲取FirebaseAuth對象的共享實例
+        auth = Firebase.auth
     }
 }
